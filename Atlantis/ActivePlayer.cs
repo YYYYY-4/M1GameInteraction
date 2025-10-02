10ca913537f20a01de116cb3cef2140edf964f32
Ruby van den Heuvel <merijn.van.den.heuvel1@student.nhlstenden.com>
2025-10-02 13:15:52 +0200

    Created PlayerSave and ActivePlayer classes with basic functionality

---
+++ Atlantis/ActivePlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atlantis
+{
+    internal class ActivePlayer
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public ActivePlayer(PlayerSave save)
+        {
+            name = save.Name;
+        }
+    }
+}
