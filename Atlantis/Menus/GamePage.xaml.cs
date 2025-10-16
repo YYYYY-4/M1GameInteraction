303d2c6bcf3c3ccf5372dc7b9d4a4ad43c4c4d68
JelleHoekstra-BOX <jelle.hoekstra1@student.nhlstenden.com>
2025-10-16 16:31:56 +0200

    Ui start

--- Atlantis/Menus/GamePage.xaml.cs
+++ Atlantis/Menus/GamePage.xaml.cs
@@ -12,7 +12,6 @@ namespace Atlantis.Menus;
 public partial class GamePage : Page
 {
     MainWindow _window;
-    Page _page;
     Canvas _canvas;
     GameScene? _scene;
 
@@ -27,31 +26,37 @@ public partial class GamePage : Page
         _window = window;
         _save = save;
         LoadScene<DemoLevel>();
+        Win(1); 
     }
-
 
     /// <summary>
     /// Everything that happens when you win a level
     /// </summary>
     /// <param name="level"></param>
     /// <param name="name"></param>
-    public void win(int level, string name)
+    public void Win(int level)
     {
         int score = Score.Calculation();
-        HighscorePage.AddRecord(level, score, name);
+        HighscorePage.AddRecord(level, score, _save.Name);
+
+        HigscoreTable.ItemsSource = HighscorePage.ReadData(level);
     }
     
     /// <typeparam name="T">Scene which inherits Page and defines a Canvas at it's root.</typeparam>
     public void LoadScene<T>() where T : Page
     {
         if (_scene != null)
+        {
             _scene.Destroy();
-        
-        _page = new DemoLevel();
-        _canvas = (Canvas) _page.Content;
+            RootGrid.Children.Remove(_canvas);
+        }
+
+        Score = new GameScore();
+        var page = new DemoLevel();
+        _canvas = (Canvas)page.Content;
         _scene = new GameScene(_window, this, _canvas);
-        
-        _page.Content = null;
-        Content = _canvas;
+
+        page.Content = null;
+        RootGrid.Children.Add(_canvas);
     }
 }
