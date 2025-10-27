e41b83e950461e24386d7288b012ff8e88f45e74
[jesse-ict] <[jesse.van.der.meer@student.nhlstenden.com]>
2025-10-27 12:39:49 +0100

    functies voor sfx toegevoegd

--- Atlantis/Game/Sounds.cs
+++ Atlantis/Game/Sounds.cs
@@ -16,7 +16,9 @@ namespace Atlantis.Game
         private static MediaPlayer mainMusic = new MediaPlayer();
         private static bool isInitialized = false;
         private static double musicVolume = double.Parse(File.ReadAllText("MusicSettings.txt")) / 100;
-        private double sfxVolmume = double.Parse(File.ReadAllText("SfxSettings")) / 100; 
+        private double sfxVolume = double.Parse(File.ReadAllText("SfxSettings")) / 100;
+
+        private MediaPlayer loopedSfxPlayer;
 
         //Plays soundtrack.mp3 and loops the music
         public static void PlayMainMusicLoop()
@@ -38,27 +40,19 @@ namespace Atlantis.Game
             mainMusic.Play();
         }
 
-        //stops Music (not unload)
-        public static void StopMainMusicLoop()
-        {
-            mainMusic.Stop();
-        }
+        //stops Music
+        public static void StopMainMusicLoop() { mainMusic.Stop(); }
 
         //Pauses 
-        public static void PauseMainMusicLoop()
-        {
-            mainMusic.Pause();
-        }
+        public static void PauseMainMusicLoop() { mainMusic.Pause(); }
 
         //continues the music (done after it has been pauzed or stopped)
-        public static void ContinueMainMusicLoop()
-        {
-            mainMusic.Play();
-        }
+        public static void ContinueMainMusicLoop() { mainMusic.Play(); }
 
-        //Closes/Unloads the music so it doesnt take up resources 
-        public static void CloseMainMusicLoop()
+        //Closes/unloads the musicloop 
+        public static void CloseMainMusicLoop() 
         {
+            mainMusic.Stop();
             mainMusic.Close();
         }
 
@@ -67,6 +61,44 @@ namespace Atlantis.Game
         {
             musicVolume = double.Parse(File.ReadAllText("MusicSettings.txt"));
             mainMusic.Volume = musicVolume;
+        }
+
+        //play any sound effect
+        public void PlaySfx(string filePath)
+        {
+            MediaPlayer sfxPlayer = new MediaPlayer();
+            sfxPlayer.Open(new Uri(filePath, UriKind.RelativeOrAbsolute));
+            sfxPlayer.Volume = sfxVolume;
+            sfxPlayer.Play();
+
+            // Cleanup when finished
+            sfxPlayer.MediaEnded += (s, e) =>
+            {
+                sfxPlayer.Close();
+            };
+        }
+
+        // play looping sound effect
+        public void PlaySfxLoop(string filePath)
+        {
+            loopedSfxPlayer = new MediaPlayer();
+            loopedSfxPlayer.Open(new Uri(filePath, UriKind.RelativeOrAbsolute));
+            loopedSfxPlayer.Volume = sfxVolume;
+
+            loopedSfxPlayer.MediaEnded += (s, e) =>
+            {
+                loopedSfxPlayer.Position = TimeSpan.Zero;
+                loopedSfxPlayer.Play();
+            };
+
+            loopedSfxPlayer.Play();
+        }
+
+        // closes/unloads the looping sfx
+        public void CloseSfxLoop()
+        {
+            loopedSfxPlayer.Stop();
+            loopedSfxPlayer.Close();
         }
     }
 }
