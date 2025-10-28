aede3f6e24bfb22c85633a0d809256b3940f17eb
Ruby van den Heuvel <merijn.van.den.heuvel1@student.nhlstenden.com>
2025-10-28 16:49:43 +0100

    Yes

--- Atlantis/GameScore.cs
+++ Atlantis/GameScore.cs
@@ -33,7 +33,7 @@ namespace Atlantis
         public int Calculation(float time)
         {
             int maxScore = 10000;
-            _score = maxScore - (Convert.ToInt32(time * 50)) + (collectables * 1000);
+            _score = maxScore - (Convert.ToInt32(time * 150)) + (collectables * 1000);
 
             return _score;
         }
